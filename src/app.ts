e5e1839f09ed9891b27f51dac12f0f70f6837708
Anton Ptashynskyi <aptashysnkyi@opera.com>
2025-11-19 16:41:23 +0100

    [dev] - init be project

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import cookieParser from 'cookie-parser';
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import helmet from 'helmet';
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.disable('x-powered-by');
+app.use(helmet({
+  contentSecurityPolicy: false,
+  xPoweredBy: false,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/", async (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send("Hello");
+})
+
+const init = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log("Server Listen PORT:", PORT);
+    });
+  } catch (error) {
+    console.log('Error on server init', error);
+  }
+};
+
+init();
